18a6627780f49f868437d48d0e54feb565e220a8
Manan <mananarif025@gmail.com>
2024-06-02 21:42:12 +0500

    text editor

---
+++ app.js
@@ -0,0 +1,91 @@
+
+let divEl = document.querySelectorAll(".text")
+
+let inpEl = document.querySelectorAll("input")
+
+// let pEl = document.querySelectorAll("P")[0]
+
+// pEl.innerHTML = `Hello World`
+
+
+
+function sve(){
+inpEl[0].value.className = 'clr'
+   
+}
+
+
+function bld(){
+
+    inpEl[0].className = "bld"
+   }
+
+
+
+
+function itl(){
+    // let pEl = inpEl[0].value
+
+ inpEl[0].className = "itl"
+}
+
+function remov(){
+
+    inpEl[0].innerHTML = ""
+   }
+   
+   function bodr(){
+
+    inpEl[0].className = "bodr"
+   }
+   
+   function line(){
+
+    inpEl[0].className = "line"
+   }
+   function ovLin(){
+
+    inpEl[0].className = "over"
+   }
+   function thr(){
+
+    inpEl[0].className = "lin-thr"
+   }
+
+
+   function upper(){
+    inpEl[0].className = 'upper'
+   }
+   
+   function lower(){
+    inpEl[0].className = 'lower'
+   }
+   function capital(){
+    inpEl[0].className = 'capital'
+   }
+
+function red(){
+    inpEl[0].className = 'red'
+
+}
+
+function blue(){
+    inpEl[0].value.className = 'blue'
+
+}
+
+function yellow(){
+    inpEl[0].className = 'yellow'
+
+}
+
+
+
+
+
+
+
+
+// function remov(){
+//     inpEl.value = ''
+// }
